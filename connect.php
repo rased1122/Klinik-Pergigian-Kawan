c1b4532b694674d3aa6f4b9041bf10583cffebdc
rashid <55909138+rased1122@users.noreply.github.com>
2024-07-22 14:08:49 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+						$dbhost = "localhost";
+						$dbusername = "root";
+						$dbpassword = "";
+						$dbname = klinik pergigian kawan";
+						
+					if	($conn = mysqli_connect ($dbhost, $dbusername, $dbpassword, $dbname))
+{				
+					die("failed to coonect!);
+}
